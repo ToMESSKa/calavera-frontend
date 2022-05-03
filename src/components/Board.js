bf79b9772be75606b380b6e3622218dec6d1f9b8
Kovacs Tamas <kovacstamas990@gmail.com>
2022-05-03 20:26:46 +0200

    Board component is created

---
+++ src/components/Board.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function Board () {
+
+    return(
+    <div className="Board">
+      <header className="Board-header">
+          Calavera
+      </header>
+    </div>
+    );
+}
+export default Board;
