0c24629272cf6ddae55c15c7dbd558b88e65cff3
Kovacs Tamas <kovacstamas990@gmail.com>
2022-05-03 21:27:23 +0200

    Board is created with button displaying cells

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,12 +1,25 @@
-import React from "react";
+import React, {useState, useEffect } from "react";
+import Cell from "./Cell";
+import {Card, Space, Col, Row, Button} from "antd";
 
 
 function Board () {
 
+    const [orangeCells, setOrangecells] = useState([]);
+
+    const createBoard = () =>{
+        setOrangecells([1,2,3,4])
+        }
+
     return(
     <div className="Board">
       <header className="Board-header">
-          Calavera
+          <button onClick={(event) => {
+              createBoard(event);
+            }}>CLICK HERE</button>
+          <Row>
+          {orangeCells.map((orangeCell, i) => <Col><Cell></Cell></Col>)}
+          </Row>
       </header>
     </div>
     );
---
+++ src/components/Cell.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function Cell () {
+
+    return(
+    <div className="Cell">
+        CAL
+    </div>
+    );
+}
+export default Cell;
