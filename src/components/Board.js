8aef81c83585a7db3a5ee86be20ff311a631b2cb
Kovacs Tamas <kovacstamas990@gmail.com>
2022-05-10 20:43:47 +0200

    Basic grid is created

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect } from "react";
 import Cell from "./Cell";
 import {Card, Space, Col, Row, Button} from "antd";
+import "antd/dist/antd.css";
 
 
 function Board () {
@@ -10,14 +11,19 @@ function Board () {
     const createBoard = () =>{
         setOrangecells([1,2,3,4])
         }
+    
+    const rows = [1,2,3,4];
+    const columns = [1,2, 3, 4, 5, 6, 7, 8, 9, 10];
 
     return(
     <div className="Board">
-      <header className="Board-header">
-          <Row>
-          <Col><Cell></Cell></Col>
-          </Row>
-      </header>
+      {rows.map((row, rowindex) => (
+        <Row>
+          {columns.map((column, columnindex) => (
+        <Col><Cell></Cell></Col>
+      ))}
+        </Row>
+      ))}
     </div>
     );
 }
