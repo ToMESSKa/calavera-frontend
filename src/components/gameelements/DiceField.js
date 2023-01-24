18bdd3a045900502a53f72e23864e21d29b93a33
Kovacs Tamas <kovacstamas990@gmail.com>
2023-01-24 18:41:24 +0100

    Cell mark bug is fixed. Variables are renamed in markCellsFunction

--- src/components/gameelements/DiceField.js
+++ src/components/gameelements/DiceField.js
@@ -62,7 +62,6 @@ function DiceField(props) {
   });
   const [dicesVisible, setDicesVisible] = useState(true);
   const [rollingIsOver, setRollingIsOver] = useState(false);
-  const [diceActionAfterRoll, setDiceActionAfterRoll] = useState();
   const [stopButtonVisible, setStopButtonVisible] = useState(false);
   const [selectedDicesForReroll, setSelectedDicesForReroll] = useState({
     purple: [],
@@ -74,13 +73,14 @@ function DiceField(props) {
   });
   const [rerollButtonVisible, setRerollButtonVisible] = useState(false);
   const [numberOfRolledDices, setNumberOfRolledDices] = useState(6);
-  const [orangeCellsToMark, setOrangeCellsToMark] = useState(0);
-  const [purpleCellsToMark, setRedCellsToMark] = useState(0);
-  const [turquoiseCellsToMark, setTurquoiseCellsToMark] = useState(0);
-  const [blackCellsToMark, setBlackCellsToMark] = useState(0);
+  const [markedOrangeCellsCounter, setOrangeCellsToMark] = useState(0);
+  const [markedPurpleCellsCounter, setRedCellsToMark] = useState(0);
+  const [markedTurquoiseCellsCounter, setTurquoiseCellsToMark] = useState(0);
+  const [markedBlackCellsCounter, setBlackCellsToMark] = useState(0);
   const [playerToMarkCells, setPlayerToMarkCells] = useState(1);
   const [mainPlayerTurnIsOver, setMainPlayerTurnIsOver] = useState(false);
   const [isTurnOver, setTurnOver] = useState(false);
+  const [stopButtonDisabled, setstopButtonDisabled] = useState(false);
   const faces = [purple, black, orange, rose, skull, turquoise];
   let counter = 0;
   let selectedDiceColorToMarkCellIsSent = false;
@@ -133,6 +133,7 @@ function DiceField(props) {
 
   useSubscription("/topic/getmarkedcells", (message) => {
     if (props.playerIDForGame !== playerToMarkCells) {
+      console.log(JSON.parse(message.body));
       selectedDiceColorToMarkCellIsSent = true;
       selectDiceColorToMarkCells(JSON.parse(message.body).value);
       selectedDiceColorToMarkCellIsSent = false;
@@ -162,7 +163,7 @@ function DiceField(props) {
       //Handle error
     }
     groupDicesByColor(dicesGroupedByColor, diceRollResults.diceRolls);
-    setDicesGroupedByColor(dicesGroupedByColor);
+    setDicesGroupedByColor((diceGroup) => ({ ...diceGroup }));
     setDicesVisible(false);
     setStopButtonVisible(true);
   };
@@ -171,17 +172,6 @@ function DiceField(props) {
     if (stompClient) {
       stompClient.publish({
         destination: "/app/selecteddiceforreroll",
-        body: JSON.stringify(dice),
-      });
-    } else {
-      //Handle error
-    }
-  };
-
-  const sendCanceledDice = (dice) => {
-    if (stompClient) {
-      stompClient.publish({
-        destination: "/app/canceleddice",
         body: JSON.stringify(dice),
       });
     } else {
@@ -201,18 +191,8 @@ function DiceField(props) {
     }
   };
 
-  const sendSelectedColor = (dice) => {
-    if (stompClient) {
-      stompClient.publish({
-        destination: "/app/selectedcolor",
-        body: JSON.stringify(dice),
-      });
-    } else {
-      //Handle error
-    }
-  };
-
   const sendMarkedCells = (markedCells) => {
+    console.log(markedCells);
     if (stompClient) {
       stompClient.publish({
         destination: "/app/markedcells",
@@ -260,7 +240,6 @@ function DiceField(props) {
         });
       }
       if (diceRollResults.diceRolls.length === numberOfRolledDices) {
-        console.log(diceRollResults)
         sendRollResultsAndGroupThem();
         setDiceRollResults({ diceRolls: [] });
       }
@@ -278,9 +257,10 @@ function DiceField(props) {
   const groupForTheOtherPlayer = (rolls) => {
     if (props.playerIDForGame === 2) {
       groupDicesByColor(dicesGroupedByColor, rolls);
-      setDicesGroupedByColor(dicesGroupedByColor);
+      setDicesGroupedByColor((diceGroup) => ({ ...diceGroup }));
       setDicesVisible(false);
       setStopButtonVisible(true);
+      setstopButtonDisabled(true);
     }
   };
 
@@ -406,9 +386,18 @@ function DiceField(props) {
     setDicesToBeRolled(newDices);
     setSelectedDicesForReroll(defaultDicesGroupedByColor);
     setDicesVisible(true);
-    diceReferences.splice(newDices.diceRolls.length)
+    diceReferences.splice(newDices.diceRolls.length);
     setDiceReferences(diceReferences);
-    setNumberOfRolledDices(newDices.diceRolls.length)
+    setNumberOfRolledDices(newDices.diceRolls.length);
+    increaseRollCounter();
+  };
+
+  const increaseRollCounter = () => {
+    if (props.rerollCounter === "first") {
+      props.setRerollCounter("second");
+    } else if (props.rerollCounter === "second") {
+      props.setRerollCounter("third");
+    }
   };
 
   const prepareForReRoll = () => {
@@ -416,24 +405,6 @@ function DiceField(props) {
     setDiceFieldForReroll({ diceRolls: newDicesToBeRolled });
     setRerollButtonVisible(false);
     sendNewDicesForRoll(newDicesToBeRolled);
-    //   setSelectedDicesForReroll(defaultDicesGroupedByColor);
-    //   setNumberOfRolledDices(selectedDicesForReroll.length);
-    //   setDicesVisible(true);
-    //   setRerollButtonVisible(false);
-    //   if (props.playerIDForGame === 1) {
-    //     sendNewDiceForRoll({ diceRolls: selectedDicesForReroll });
-    //   }
-    //   if (props.rerollCounter === 1) {
-    //     props.setRerollCounter("second");
-    //   } else if (props.rerollCounter === "second") {
-    //     props.setRerollCounter("third");
-    //   }
-    //   let newAllDiceToRoll = [];
-    //   for (let i = 0; i < selectedDicesForReroll.length; i++) {
-    //     newAllDiceToRoll.push(allDiceToRoll[i]);
-    //   }
-    //   setAllDiceToRoll(newAllDiceToRoll);
-    //   setDiceActionAfterRoll(selectDiceForReroll);
   };
 
   const endRollingPhase = (value) => {
@@ -441,55 +412,55 @@ function DiceField(props) {
     setRollingIsOver(true);
   };
 
-  const markCells = (numberOfDice, value) => {
+  const markCells = (numberOfDices, diceColor) => {
     let cells = [];
-    let cellsToMark = defineColor(value);
+    let numberOfMarkedCells = getNumberOfMarkedCells(diceColor);
     for (let i = 0; i < 13; i++) {
-      if (i < cellsToMark + numberOfDice) {
+      if (i < numberOfMarkedCells + numberOfDices) {
         cells.push("X");
       } else {
         cells.push("");
       }
     }
     if (props.playerIDForGame === 1 && playerToMarkCells === 1) {
-      selectPlayer(value, 1, cells);
+      setMarkedCells(diceColor, 1, cells);
     } else if (props.playerIDForGame === 1 && playerToMarkCells === 2) {
-      selectPlayer(value, 2, cells);
+      setMarkedCells(diceColor, 2, cells);
     } else if (props.playerIDForGame === 2 && playerToMarkCells === 1) {
-      selectPlayer(value, 1, cells);
+      setMarkedCells(diceColor, 1, cells);
     } else if (props.playerIDForGame === 2 && playerToMarkCells === 2) {
-      selectPlayer(value, 2, cells);
+      setMarkedCells(diceColor, 2, cells);
     }
   };
 
-  const defineColor = (value) => {
-    if (value === 1) {
-      return purpleCellsToMark;
-    } else if (value === 2) {
-      return blackCellsToMark;
-    } else if (value === 3) {
-      return orangeCellsToMark;
-    } else if (value === 6) {
-      return turquoiseCellsToMark;
+  const getNumberOfMarkedCells = (diceColor) => {
+    if (diceColor === 1) {
+      return markedPurpleCellsCounter;
+    } else if (diceColor === 2) {
+      return markedBlackCellsCounter;
+    } else if (diceColor === 3) {
+      return markedOrangeCellsCounter;
+    } else if (diceColor === 6) {
+      return markedTurquoiseCellsCounter;
     }
   };
 
-  const selectPlayer = (value, player, cells) => {
-    if (player === 1 && value === 1) {
+  const setMarkedCells = (diceColor, player, cells) => {
+    if (player === 1 && diceColor === 1) {
       props.setPlayerOnePurpleCells(cells);
-    } else if (player === 1 && value === 2) {
+    } else if (player === 1 && diceColor === 2) {
       props.setPlayerOneBlackCells(cells);
-    } else if (player === 1 && value === 3) {
+    } else if (player === 1 && diceColor === 3) {
       props.setPlayerOneOrangeCells(cells);
-    } else if (player === 1 && value === 6) {
+    } else if (player === 1 && diceColor === 6) {
       props.setPlayerOneTurquoiseCells(cells);
-    } else if (player === 2 && value === 1) {
+    } else if (player === 2 && diceColor === 1) {
       props.setPlayerTwoPurpleCells(cells);
-    } else if (player === 2 && value === 2) {
+    } else if (player === 2 && diceColor === 2) {
       props.setPlayerTwoBlackCells(cells);
-    } else if (player === 2 && value === 3) {
+    } else if (player === 2 && diceColor === 3) {
       props.setPlayerTwoOrangeCells(cells);
-    } else if (player === 2 && value === 6) {
+    } else if (player === 2 && diceColor === 6) {
       props.setPlayerTwoTurquoiseCells(cells);
     }
   };
@@ -497,8 +468,9 @@ function DiceField(props) {
   const selectDiceColorToMarkCells = (selectedDiceColor) => {
     let numberOfCellsToMark =
       deleteDiceGroupByColorAndGetNumberOfCellsToMark(selectedDiceColor);
+    console.log(numberOfCellsToMark);
     markCells(numberOfCellsToMark, selectedDiceColor);
-    setDicesGroupedByColor(dicesGroupedByColor);
+    setDicesGroupedByColor((diceGroup) => ({ ...diceGroup }));
     if (!selectedDiceColorToMarkCellIsSent) {
       let markedCells = {
         numberOfDice: numberOfCellsToMark,
@@ -511,6 +483,8 @@ function DiceField(props) {
 
   const deleteDiceGroupByColorAndGetNumberOfCellsToMark = (diceColor) => {
     let numberOfCellsToMark = 0;
+    console.log(diceColor);
+    console.log(dicesGroupedByColor);
     for (const [key] of Object.entries(dicesGroupedByColor)) {
       if (
         dicesGroupedByColor[key][0] !== undefined &&
@@ -553,7 +527,9 @@ function DiceField(props) {
         </Col>
         <Col>
           {stopButtonVisible ? (
-            <button onClick={endRollingPhase}>STOP</button>
+            <button disabled={stopButtonDisabled} onClick={endRollingPhase}>
+              STOP
+            </button>
           ) : (
             <div></div>
           )}
